2ca885ebd7eac1d041f5c3bcd4402cf99b0670f0
atomkern <81935062+atom1488@users.noreply.github.com>
2021-05-13 23:22:55 +0000

    Oscar Tango Alpha November

---
+++ otan.py
@@ -0,0 +1,15 @@
+def Otan():
+    try:
+        print("Ecrivez le texte à transcrire en OTAN : ")
+        array = {' ': ' ', 'A': 'Alpha', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo', 'F': 'Foxtrot',
+                'G': 'Golf', "H": "Hotel", 'I': 'India', 'J': 'Juliet', 'K': 'Kilo', 'L': 'Lima', 'M': 'Mike',
+                'N': 'November', 'O': 'Oscar', 'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo', 'S': 'Sierra', 'T': 'Tango',
+                'U': 'Uniform', 'V': 'Victor', 'W': 'Whiskey', 'X': 'Xray', 'Y': 'Yankee', 'Z': 'Zulu'}
+        otanInput = input('\n').upper()
+        if(any(chr.isdigit() for chr in otanInput)):
+            return print('\nVeuillez ne pas mettre de nombres.')
+        translate = list(map(array.get, otanInput))
+        print("\n" + " ".join(map(array.get, otanInput)) + "\n")
+    except Exception as err:
+        print(f"[ERREUR]: {err}")
+Otan()
